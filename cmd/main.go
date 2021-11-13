55a44e889c51ee997808c6d7d6ab3f0d401de813
iskiy <vasyarosada@gmail.com>
2021-11-13 15:51:58 +0200

    petri-net

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,58 @@
-package cmd
+package main
+
+import (
+	. "coloured-petri-net/pkg"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	blue  = "blue"
+	green = "green"
+	red   = "red"
+)
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func main() {
+	place0 := Place{PlacesID: 0, Tokens: []string{red, green, blue}}
+	place1 := Place{PlacesID: 1, Tokens: []string{red, green, blue}}
+	place2 := Place{PlacesID: 2, Tokens: []string{}}
+	place3 := Place{PlacesID: 3, Tokens: []string{}}
+	place4 := Place{PlacesID: 4, Tokens: []string{}}
+	place5 := Place{PlacesID: 5, Tokens: []string{}}
+
+	var transition0 = Transition{
+		TransitionID: 0,
+		FromArcs:     []Arc{{red, 0, 0}},
+		ToArcs:       []Arc{{red, 0, 3}},
+	}
+	var transition1 = Transition{
+		TransitionID: 1,
+		FromArcs: []Arc{{blue, 1, 1},
+			{green, 1, 1},
+			{blue, 0, 1},
+		},
+		ToArcs: []Arc{{red, 1, 2}},
+	}
+	var transition2 = Transition{
+		TransitionID: 2,
+		FromArcs: []Arc{{red, 2, 2},
+			{red, 3, 2},
+		},
+		ToArcs: []Arc{{blue, 2, 4},
+			{green, 2, 5},
+		},
+	}
+	petriNet := PetriNet{Places: []Place{place0, place1, place2, place3, place4, place5},
+		Transitions: []Transition{transition0, transition1, transition2}}
+	fmt.Println(petriNet.FindExecTransitions())
+	fmt.Println(petriNet)
+	fmt.Println("\n")
+	petriNet.Run()
+
+	fmt.Println(petriNet)
+}
--- pkg/cp_net.go
+++ pkg/cp_net.go
@@ -1,1 +1,163 @@
 package pkg
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+type Place struct {
+	PlacesID int
+	Tokens   []string
+}
+
+func NewPlace(placesID int, tokens []string) *Place {
+	fmt.Println(tokens)
+	pl := &Place{PlacesID: placesID, Tokens: tokens}
+	pl.Tokens = tokens
+	return pl
+}
+
+func (p *Place) containsColour(colour string) (int, bool) {
+	for i, t := range p.Tokens {
+		if t == colour {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+type Arc struct {
+	Colour string
+	// IDs place/transition
+	FromID int
+	ToID   int
+}
+
+func (a Arc) fromArcString() string {
+	return fmt.Sprintf("colour: %s, from placeID: %d", a.Colour, a.FromID)
+}
+
+func (a Arc) toArcString() string {
+	return fmt.Sprintf("colour: %s, to placeID: %d", a.Colour, a.ToID)
+}
+
+type Transition struct {
+	TransitionID int
+	ToArcs       []Arc
+	FromArcs     []Arc
+}
+
+func (t Transition) String() string {
+	var res string
+	res += fmt.Sprintf("TransitionID: %d", t.TransitionID)
+	res += "\n\t\tFromArcs: "
+	for _, a := range t.FromArcs {
+		res += fmt.Sprintf("\n\t\t\t %s;", a.fromArcString())
+	}
+	res += "\n\t\tToArcs: "
+	for _, a := range t.ToArcs {
+		res += fmt.Sprintf("\n\t\t\t %s;", a.toArcString())
+	}
+	return res
+}
+
+type PetriNet struct {
+	Places      []Place
+	Transitions []Transition
+}
+
+func (pn PetriNet) String() string {
+	var res = "Places: "
+	for _, p := range pn.Places {
+		res += fmt.Sprintf("\n\t ID: %d, Tokens: [%s]", p.PlacesID, strings.Join(p.Tokens, " "))
+	}
+	res += "\nTransitions:"
+	for _, t := range pn.Transitions {
+		res += fmt.Sprintf("\n\t%s", t)
+	}
+	return res
+}
+
+func (pn *PetriNet) Run() {
+	for {
+		transToExec := pn.FindExecTransitions()
+		if len(transToExec) == 0 {
+			break
+		}
+		randomIndex := rand.Intn(len(transToExec))
+		fmt.Println(transToExec[randomIndex])
+		pn.execTransition(transToExec[randomIndex])
+	}
+}
+
+func (pn *PetriNet) execTransition(idx int) {
+	for _, a := range pn.Transitions[idx].FromArcs {
+		id, _ := pn.Places[a.FromID].containsColour(a.Colour)
+		pl, _ := subToken(id, pn.Places[a.FromID])
+		pn.Places[a.FromID] = pl
+	}
+	for _, a := range pn.Transitions[idx].ToArcs {
+		pn.Places[a.ToID].Tokens = append(pn.Places[a.ToID].Tokens, a.Colour)
+	}
+}
+
+func (pn *PetriNet) FindExecTransitions() []int {
+	var res []int
+	for i := range pn.Transitions {
+		if pn.isExecutableTransitions(i) {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func (pn PetriNet) isExecutableTransitions(idx int) bool {
+	placesCopy := createTempPlaces(pn.Places)
+	if len(placesCopy) == 0 {
+		return false
+	}
+	for _, arc := range pn.Transitions[idx].FromArcs {
+		if !pn.checkToArc(arc, placesCopy) {
+			return false
+		}
+	}
+	return true
+}
+
+func (pn PetriNet) checkToArc(arc Arc, places []Place) bool {
+	if arc.FromID >= len(pn.Places) || arc.ToID >= len(pn.Transitions) {
+		return false
+	}
+	if len((places)[arc.FromID].Tokens) == 0 {
+		return false
+	}
+	idx, ok := (places)[arc.FromID].containsColour(arc.Colour)
+	if !ok {
+		return false
+	}
+	pl, err := subToken(idx, (places)[arc.FromID])
+	if err != nil {
+		return false
+	}
+	(places)[arc.FromID] = pl
+	return true
+}
+
+func subToken(idx int, p Place) (Place, error) {
+	if idx >= len(p.Tokens) || idx < 0 {
+		return p, fmt.Errorf("idx out of bounds")
+	}
+	p.Tokens = append(p.Tokens[:idx], p.Tokens[idx+1:]...)
+	return p, nil
+}
+
+func createTempPlaces(places []Place) []Place {
+	var res []Place
+	for _, p := range places {
+		var tokensTemp = make([]string, len(p.Tokens))
+		copy(tokensTemp, p.Tokens)
+		res = append(res, Place{PlacesID: p.PlacesID, Tokens: tokensTemp})
+	}
+	return res
+}
